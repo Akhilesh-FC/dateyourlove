8ac9e84c07a57486c8761e2bd24d136eb6a7b072
Akhilesh-FC <akhilesh@foundercodes.com>
2026-07-24 15:39:23 +0530

    option controller

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/Api/userController');
-const OptionsController = require('../controllers/Api/OptionsController');
+const optionsController = require('../controllers/Api/optionscontroller');
 const upload = require('../middleware/upload');
 
 router.get('/', (req, res) => res.json({ ok: true, version: 'api-v1' }));
@@ -14,13 +14,13 @@ router.post('/otp/verify', userController.verifyOtp); // JSON body, photos as UR
 router.post('/register', upload.array('photos', 6), userController.registerUser);
 
 // ---------- DROPDOWN / OPTIONS - har list ki apni alag api ----------
-router.get('/options/looking-for', OptionsController.getLookingForOptions);
-router.get('/options/religions', OptionsController.getReligionOptions);
-router.get('/options/languages', OptionsController.getLanguageOptions);
-router.get('/options/smoking', OptionsController.getSmokingOptions);
-router.get('/options/drinking', OptionsController.getDrinkingOptions);
-router.get('/options/workout', OptionsController.getWorkoutOptions);
-router.get('/options/diet', OptionsController.getDietOptions);
+router.get('/options/looking-for', optionsController.getLookingForOptions);
+router.get('/options/religions', optionsController.getReligionOptions);
+router.get('/options/languages', optionsController.getLanguageOptions);
+router.get('/options/smoking', optionsController.getSmokingOptions);
+router.get('/options/drinking', optionsController.getDrinkingOptions);
+router.get('/options/workout', optionsController.getWorkoutOptions);
+router.get('/options/diet', optionsController.getDietOptions);
 
 // ---------- PROTECTED (add here once you build them) ----------
 // router.get('/profile/me', authMiddleware, userController.getMyProfile);
